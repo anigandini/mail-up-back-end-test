02bdb73e84b2a4d361fda2e898499a144ccd6ce0
anigandini <anigandini@gmail.com>
2022-07-13 15:40:13 -0300

    project set up & products migration

---
+++ database/migrations/2022_07_13_173403_products.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->string('brand');
+            $table->integer('price');
+            $table->integer('price_sale');
+            $table->string('category');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('products');
+    }
+}
